7e355626ebc69d33165576b8a0f60ae18f438650
y Riyazi <119464932+yriyazi@users.noreply.github.com>
2024-07-21 16:00:58 +0330

    Update testRedis.sh

--- testRedis.sh
+++ testRedis.sh
@@ -3,6 +3,9 @@
 # "exec bash" keeps terminal open even if the program is finished.
 gnome-terminal -- bash -c "python3 setRedis.py; exec bash"
 
+# Make getRedis executable
+chmod +x ./getRedis
+
 # Run the compiled C++ program (getRadis) in new terminal to get the variable's value :
 gnome-terminal -- bash -c "./getRedis; exec bash"
 
